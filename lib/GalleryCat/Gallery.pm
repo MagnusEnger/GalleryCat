131333ac7ffbbc9a96caae659a940745be44a515
Todd Holbrook <todd@dreamtheorystudios.com>
2010-06-11 18:24:12 -0700

    Exposed useful_keywords to Gallery

--- lib/GalleryCat/Gallery.pm
+++ lib/GalleryCat/Gallery.pm
@@ -143,7 +143,7 @@ has 'images_store_config' => (
 has 'images_store' => (
     is          => 'rw',
     isa         => 'Object',
-    handles     => [ qw( images_count max_image_width max_image_height keywords ) ],
+    handles     => [ qw( images_count max_image_width max_image_height keywords useful_keywords ) ],
 
 );
 
